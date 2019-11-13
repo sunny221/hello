50f963f8b5e50c73c560aec64dc924794cbb226c
sunny221 <55087772+sunny221@users.noreply.github.com>
2019-11-13 16:54:46 +0800

    第九周作业
    第九周作业

---
+++ Student.java
@@ -0,0 +1,30 @@
+package jiu;
+
+public class Student {
+	private String studentNum;
+	private String studentName;
+	private String studentgender;
+	public String getStudentNum() {
+		return studentNum;
+	}
+	public void setStudentNum(String num) {
+		this.studentNum=num;
+	}
+	public String getStudentName() {
+		return studentName;
+	}
+	public void setStudentName(String studentName) {
+		this.studentName =studentName;
+	}
+	public String getStudentgender() {
+		return studentgender;
+	}
+	public void setStudentgender(String gender) {
+		this.studentgender =gender;
+	}
+	public String toString() {
+		return "学号："+this.getStudentNum()+"\n名字："+this.getStudentName()+"\n性别："+this.getStudentgender();
+		
+	}
+	
+}
