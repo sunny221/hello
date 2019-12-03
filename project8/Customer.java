760dcfb6ffe80a3736fcaec026616fc742236bf4
sunny221 <55087772+sunny221@users.noreply.github.com>
2019-12-03 23:05:07 +0800

    改frequentRenterPoints参数
    改frequentRenterPoints参数

---
+++ project8/Customer.java
@@ -0,0 +1,42 @@
+package project8;
+ import java.util.Enumeration;
+ import java.util.Vector;
+ class Customer{
+ 	private String _name;
+ 	private Vector _rentals = new Vector();
+ 	public Customer(String name){
+ 		_name = name;
+ 	};
+ 	public void addRental(Rental arg){
+ 		_rentals.addElement(arg);
+ 	}
+ 	public String getName (){
+ 		return _name;
+ 	}
+ 	public String statement(){
+ 		double totalAmount = 0;
+ 		int frequentRenterPoints = 0;
+ 		Enumeration rentals = _rentals.elements();
+ 		String result = "Rental Record for " + getName() + "\n";
+ 		while (rentals.hasMoreElements()){
+ 			Rental each = (Rental) rentals.nextElement();
+ 			//double thisAmount = 0;
+ 			//Rental each = (Rental) rentals.nextElement();
+ 			//thisAmount = each.getCharge();
+ 			frequentRenterPoints += each.getFrequentRenterPoints();
+
+ 			//if((each.getMovie().getPriceCode()==Movie.NEW_RELEASE)&&each.getDaysRented()>1)frequentRenterPoints++;
+ 			result +="\t"+each.getMovie().getTitle()+"\t"+
+ 			String.valueOf(each.getCharge())+"\n";
+ 			totalAmount +=each.getCharge();
+ 		}
+ 		result +="Amount owed is "+ String.valueOf(totalAmount) +"\n";
+ 		result +="You earned " + String.valueOf(frequentRenterPoints)+
+ 				"frequent renter points";
+ 		return result;
+ 	
+ 	}
+ 	private double amountFor(Rental aRental){
+ 		return aRental.getCharge();}
+ 	}
+
