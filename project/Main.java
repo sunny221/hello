dc1630aa81e00c99b25bc4d705952a991f4818cf
sunny221 <55087772+sunny221@users.noreply.github.com>
2019-12-03 22:13:00 +0800

    Delete Main.java

--- project/Main.java
+++
@@ -1,8 +0,0 @@
-package project;
- public class Main{
- 	  public static void main(String[] args) {
- 	      Customer c1=new Customer("LiMing");
- 	      c1. addRental(new Rental(new Movie("Jack",2),6));
- 	      System. out.print(c1.statement());
- 	  }
- 	} 
