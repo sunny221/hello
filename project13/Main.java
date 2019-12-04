4c64fadf8ccfaf1b08198f95994a397b587271d6
sunny221 <55087772+sunny221@users.noreply.github.com>
2019-12-04 22:10:26 +0800

    新建Price类，并在Price类内加入一个抽象函数
    新建Price类，并在Price类内加入一个抽象函数

---
+++ project13/Main.java
@@ -0,0 +1,8 @@
+package project13;
+ public class Main{
+ 	  public static void main(String[] args) {
+ 	      Customer c1=new Customer("LiMing");
+ 	      c1. addRental(new Rental(new Movie("Jack",2),6));
+ 	      System. out.print(c1.statement());
+ 	  }
+ 	}  
